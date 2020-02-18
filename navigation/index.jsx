c99ad4fbf5be24e0528856c14e3bbf0460a4e6a9
Atras Tudhi <atras.r@gmail.com>
2020-02-19 00:55:43 +0700

    buttomtab sample

--- navigation/index.jsx
+++ navigation/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 import Home from '../screens/Home';
 import Profile from '../screens/Profile';
@@ -9,7 +10,24 @@ const Tab = createBottomTabNavigator();
 
 const Tabs = () => (
   <NavigationContainer>
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused ? 'ios-heart' : 'ios-heart';
+          } else if (route.name === 'Profile') {
+            iconName = focused ? 'ios-contact' : 'ios-contact';
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+      }}
+    >
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Profile" component={Profile} />
     </Tab.Navigator>
